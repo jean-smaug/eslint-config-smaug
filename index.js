f2026fb9fa5517fff1a4297c6b7c4884b5a66f17
maxime blanc <maximeblanc.dev@gmail.com>
2018-01-06 20:58:15 +0100

    add deps

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports = {
-  extends: "eslint:recommended"
-};
+module.exports = Object.assign({}, require("./eslintrc.json"), {
+  extends: [require.resolve("eslint-config-standard-jsx")]
+});
